140fd36b3c8ed5f43b6bf07b479b93722c0bda62
Engrbrain <Engrbrain@gmail.com>
2017-12-20 14:53:10 +0100

    remove all other projects and created a single MVC project

---
+++ ZenGrantsManager/ZenGrantService/Models/Organization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZenGrantService.Models
+{
+    public class Organization
+    {
+        public int ID { get; set; }
+        public string OrgName { get; set; }
+        public string OrgAddress { get; set; }
+        public string OrgState { get; set; }
+        public string OrgCountry { get; set; }
+        public string OrgPhone { get; set; }
+        public string OrgEmail { get; set; }
+        public string OrgWebsite { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool isDeleted { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Organization()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
